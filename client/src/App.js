efde9ddc6443aabebb83c9a65ecaff59588d2bd7
Naseem Shah <naseemshahvarikodan@gmail.com>
2021-01-20 22:42:00 +0530

    set up routes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import {useState} from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+
 import './App.css';
 
+import AuthPage from './components/pages/AuthPage'
+import Menu from './components/pages/Menu'
+
 function App() {
+  let [isAuthenticated,setIsAuthenticated] =  useState(true)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/auth">
+            <AuthPage />
+          </Route>
+          <Route exact path="/menu">
+            <Menu />
+          </Route>
+          <Route path="/">
+            {isAuthenticated ? <Redirect to='/menu' /> : <Redirect to='/auth' /> }
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ client/src/components/pages/AuthPage/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AuthPage() {
+  return (
+    <>
+      <h1>Auth Page</h1>
+    </>
+  );
+}
+
+export default AuthPage;
