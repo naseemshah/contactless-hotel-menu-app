c2bca112d168aaf0b4c8cc84353cfa69a58530b5
Naseem Shah <naseemshahvarikodan@gmail.com>
2021-02-04 18:52:04 +0530

    add header

--- client/src/App.js
+++ client/src/App.js
@@ -10,25 +10,30 @@ import './App.css';
 
 import AuthPage from './components/pages/AuthPage'
 import Menu from './components/pages/Menu'
+import Header from './components/layout/Header'
 
 function App() {
-  let [isAuthenticated,setIsAuthenticated] =  useState(true)
+  let [isAuthenticated,setIsAuthenticated] =  useState(false)
   return (
-    <>
+    <div className="App"> 
+      <Header />
       <Router>
         <Switch>
           <Route exact path="/auth">
             <AuthPage />
           </Route>
+          <Route exact path="/dashboard">
+            <AuthPage />
+          </Route>
           <Route exact path="/menu">
-            <Menu />
+            {isAuthenticated ? <Menu /> : <Redirect to='/auth' /> }
           </Route>
           <Route path="/">
             {isAuthenticated ? <Redirect to='/menu' /> : <Redirect to='/auth' /> }
           </Route>
         </Switch>
       </Router>
-    </>
+    </div>
   );
 }
 
